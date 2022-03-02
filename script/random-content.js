a32740e4fb3e0b8f2b37f2e438cd1a481f8e5358
remimorvan <m.remimorvan@gmail.com>
2022-03-02 20:25:15 +0100

    m

--- script/random-content.js
+++ script/random-content.js
@@ -3,7 +3,7 @@ var funFactTitle = new Array();
 
 funFact[0] = "Contrary to popular belief, there is in fact a tenth circle of hell, reserved for people using \"et al\" in their presentations.";
 funFactTitle[0] = "Did you know?";
-funFact[1] = "In school, you might have learned that writing was invented around 3000 BC. This is fake news: <a href='https://ctan.org/pkg/knowledge?lang=en'>it was invented</a> in 2016!";
+funFact[1] = "In school, you might have learned that writing was invented around 3000 BC. This is fake news: <a href='https://ctan.org/pkg/knowledge?lang=en'>the first decent writing system</a> was invented in 2016!";
 funFactTitle[1] = "Did you know?";
 funFact[2] = "Despite many efforts, my <a href=\"https://www.irif.fr/~haberm/haindex.html\">ha-index</a> is only 64.";
 funFactTitle[2] = "Did you know?";
