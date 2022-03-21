5ef7619708a94eee9916959e9079a279dd2d08b5
remimorvan <m.remimorvan@gmail.com>
2022-03-21 18:36:14 +0100

    klcl

--- script/random-content.js
+++ script/random-content.js
@@ -7,8 +7,8 @@ funFact[1] = "In school, you might have learned that writing was invented around
 funFactTitle[1] = "Did you know?";
 funFact[2] = "Despite many efforts, my <a href=\"https://www.irif.fr/~haberm/haindex.html\">ha-index</a> is only 64.";
 funFactTitle[2] = "Did you know?";
-funFact[3] = "You can click on the background of this page to create new polygons!";
-funFactTitle[3] = "Did you know?";
+funFact[3] = "<a href='https://github.com/remimorvan/knowledge-clustering'>Knowledge-Clustering</a> is a program that helps the user write LaTeX documents using the knowledge package, by giving (helpful!) suggestions on which notions should be grouped together.";
+funFactTitle[3] = "Have you heard of?";
 funFact[4] = "<a href='https://ctan.org/pkg/knowledge'>Knowledge</a> is a LaTeX package for (among other things) automatically linking a notion to its definition via an internal hyperlink. For example, <a href='https://arxiv.org/pdf/2201.03089.pdf'>this paper</a> or <a href='https://arxiv.org/pdf/1805.06238.pdf'>this thesis</a> were written using <a href='https://ctan.org/pkg/knowledge'>knowledge</a>.";
 funFactTitle[4] = "Have you heard of?";
 funFact[5] = "<blockquote><p>Too much change is not a good thing. Ask the climate.</p><p><cite>Michael G. Scott</cite></p></blockquote><a href='https://tcs4f.org/'>Learn about Theoretical Computer Scientists for Future</a> (and stop flying to conferences)." ;
